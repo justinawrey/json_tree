0d663963a0fdbd99593a4f4daa08efb3a61de582
Justin Awrey <awreyjustin@gmail.com>
2022-09-24 11:50:10 -0700

    chore: make sample an object

--- example.ts
+++ example.ts
@@ -1,26 +1,24 @@
 import jsonTree from "./mod.ts";
 
-const data = [
-  {
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: {
-      street: "Kulas Light",
-      suite: "Apt. 556",
-      city: "Gwenborough",
-      zipcode: "92998-3874",
-      geo: {
-        lat: "-37.3159",
-        lng: "81.1496",
-      },
-    },
-    phone: {
-      area: 604,
-      code: "983-0192",
+const data = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
     },
   },
-];
+  phone: {
+    area: 604,
+    code: "983-0192",
+  },
+};
 
 console.log(jsonTree(data));
