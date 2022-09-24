d0450c881e4a1e451fbd898e8dadaa9c1c09d8e9
Justin Awrey <awreyjustin@gmail.com>
2022-09-24 09:05:57 -0700

    refactor: fix cli.ts lint

--- cli.ts
+++ cli.ts
@@ -2,17 +2,17 @@ import { jsonTree } from "./mod.ts";
 import { readJson } from "https://deno.land/std/fs/read_json.ts";
 
 if (import.meta.main) {
-  let args = Deno.args.slice();
+  const args = Deno.args.slice();
   const command = args.shift();
   switch (command) {
     case "path":
-      for (let arg of args) {
+      for (const arg of args) {
         const data = await readJson(arg);
         console.log(jsonTree(data, true));
       }
       break;
     case "fetch":
-      for (let arg of args) {
+      for (const arg of args) {
         const database = await fetch(arg);
         const data = await database.json();
         console.log(jsonTree(data, true));
